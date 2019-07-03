087beebd4e1da03b9590a65d11ca9777d769c705
Dom Henderson <dominic.henderson@softwire.com>
2019-07-03 15:31:07 +0100

    Tidying

--- catalouge.ts
+++ catalouge.ts
@@ -1,6 +1,5 @@
 import { Router } from 'express';
 import TokenHandler from './tokenHandler';
-import Book from './book';
 
 class Catalogue {
     public router;
@@ -14,12 +13,16 @@ class Catalogue {
     displayCatalogue(req,res) {
         this.db.any('SELECT * FROM public."Books"')
             .then( jsonOfBooks => {
-                    const promisesOfTitles = jsonOfBooks.map(book => this.db.any('SELECT title FROM public."BookInfo" WHERE "ISBN" = $1', book.ISBN));
-                     return Promise.all(promisesOfTitles)
-                })
-            .then(listOfBooks => res.send(listOfBooks.map(book => book[0].title).filter( (title,index,self) => (self.indexOf(title) === index)).sort().join(', ')));
-            // .then(bookList => res.send(bookList.map( book => book.title))
-            //     , ()=>  {throw new Error('Cannot map the books')});
+                const promisesOfTitles = jsonOfBooks.map(book => this.db.any('SELECT title FROM public."BookInfo" WHERE "ISBN" = $1', book.ISBN));
+                return Promise.all(promisesOfTitles)
+            })
+            .then( listOfBooks => res.send(
+                listOfBooks
+                    .map(book => book[0].title)
+                    .filter( (title,index,self) => (self.indexOf(title) === index) )
+                    .sort()
+                    .join(', '))
+            );
     }
 
     updateDataBase(db) {
