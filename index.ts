82211527dde7705bc388fcfa36f65435fe9aa2d6
Dom Henderson <dominic.henderson@softwire.com>
2019-07-03 13:44:47 +0100

    Search endpoint now works

--- index.ts
+++ index.ts
@@ -19,8 +19,8 @@ app.get('/catalogue',TokenHandler.tokenAuthentication, (req,res) => {
 });
 
 app.get('/login', (req,res) => {
-    const username = req.query.username.toLowerCase();
-    const password = req.query.password;
+    const username : string = req.query.username.toLowerCase();
+    const password : string = req.query.password;
 
     db.any('SELECT * FROM public."Users" WHERE username = $1 AND password = $2',[username,password])
         .then(data=> {
@@ -31,6 +31,44 @@ app.get('/login', (req,res) => {
             }
         })
         .catch( e => console.log(e));
+
+});
+
+app.get('/search', TokenHandler.tokenAuthentication, (req,res) => {
+    const title : string = req.query.title;
+    const author : string = req.query.author;
+
+    let condition : string;
+    if ( title === undefined ) {
+        if ( author === undefined ) {
+            res.error('No search parameters');
+            return;
+        } else {
+            condition = `author LIKE '${author}'`;
+        }
+    } else {
+        if ( author === undefined ) {
+            condition = `title LIKE '${title}'`;
+        } else {
+            condition = `author LIKE '${author}' AND title LIKE '${title}'`;
+        }
+    }
+
+    let sql : string = `SELECT * FROM public."BookInfo" as book JOIN public."AuthoredBy" as authored ON authored."ISBN" = book."ISBN" WHERE ${condition}`;
+
+    db.any(sql)
+        .then(data => {
+            let result = new Object();
+            for ( let item of data ) {
+                if ( result[item.ISBN] === undefined ) {
+                    result[item.ISBN] = { title : item.title, authors : [item.authorName] };
+                } else {
+                    result[item.ISBN].authors.push(item.authorName);
+                }
+            }
+            res.json(result);
+        })
+        .catch(e=>console.log(e));
 
 });
 
