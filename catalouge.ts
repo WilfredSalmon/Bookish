e98ba6f565007cc1516f87b7236a5bc95376d5d9
Wilfred Salmon <Wilfred.Salmon@softwire.com>
2019-07-03 16:35:17 +0100

    Added js version of catalogue

--- catalouge.ts
+++ catalouge.ts
@@ -10,22 +10,34 @@ class Catalogue {
         this.router.get('/', TokenHandler.tokenAuthentication, this.displayCatalogue.bind(this));
     }
 
-    displayCatalogue(req,res) {
-        this.db.any('SELECT * FROM public."Books"')
-            .then( jsonOfBooks => {
-                const promisesOfTitles = jsonOfBooks.map(book => {
-                    this.db.any('SELECT * FROM public."BookInfo" WHERE "ISBN" = $1', book.ISBN)
-                        .then(return )
-                });
-                return Promise.all(promisesOfTitles)
-            })
-            .then( listOfBooks => res.send(
-                listOfBooks
-                    .map(book => book[0].title)
-                    .filter( (title,index,self) => (self.indexOf(title) === index) )
-                    .sort()
-                    .join(', '))
-            );
+    private displayCatalogue(req,res) {
+        this.db.any(`SELECT Books."id", Books."ISBN", Books.available, BookInfo.title, AuthoredBy."authorName" 
+                            FROM public."Books" as Books 
+                            INNER JOIN public."BookInfo" as BookInfo ON Books."ISBN" = BookInfo."ISBN"
+                            INNER JOIN public."AuthoredBy" as AuthoredBy ON Books."ISBN" = AuthoredBy."ISBN"
+                            `)
+            .then((listOfBooks) => res.send(
+                listOfBooks.reduce((acc,book) => {
+                    if ( acc.filter( (bookInList) => bookInList.id === book.id).length >0 ) {
+                        acc.find( (bookInList) => book.id === bookInList.id).authorName += ', ' + book.authorName;
+                    } else {
+                        acc.push(book);
+                    }
+
+                    return acc;
+                },[])
+                    .reduce((acc,book) => {
+                        if (acc.filter((bookInList) => bookInList.ISBN === book.ISBN).length > 0) {
+                            const bookInList = acc.find((bookInList) => book.ISBN === bookInList.ISBN);
+                            bookInList.total += 1;
+                            if (book.available) {bookInList.available += 1}
+                        } else {
+                            acc.push({ISBN: book.ISBN, title: book.title, author: book.authorName, total:1, available: 1});
+                        }
+                        return acc;
+                    },[])
+
+            ));
     }
 
     updateDataBase(db) {
