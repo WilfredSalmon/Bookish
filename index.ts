5b86c263c78266bf918d83acee9fa4d81969e686
Dom Henderson <dominic.henderson@softwire.com>
2019-07-04 11:29:59 +0100

    Refactored login into its own file

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import TokenHandler from './tokenHandler';
+import createLoginEndpoint from './login';
 import createLoansEndpoint from './loans';
 import createSearchEndpoint from './search';
 import Catalogue from './catalouge';
@@ -20,24 +21,9 @@ TokenHandler.setUpPassportVerification(db);
 Catalogue.updateDataBase(db);
 app.use('/Catalogue', Catalogue.router);
 
+createLoginEndpoint(app,db);
 createLoansEndpoint(app,db);
 createSearchEndpoint(app,db);
 createAddBookEndpoint(app,db);
-
-app.get('/login', (req,res) => {
-    const username : string = req.query.username.toLowerCase();
-    const password : string = req.query.password;
-
-    db.any('SELECT * FROM public."Users" WHERE username = $1 AND password = $2',[username,password])
-        .then(data=> {
-            if (data.length > 0) {
-                res.send(`token is ${TokenHandler.getToken(username)}`);
-            } else {
-                res.send('invalid username and password');
-            }
-        })
-        .catch( e => console.log(e));
-
-});
 
 app.listen(port, () => console.log(`Bookish listening on port ${port}!`));
