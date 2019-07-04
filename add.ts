93a5a0344944508227c8db7c955f5fdee004cf24
Dom Henderson <dominic.henderson@softwire.com>
2019-07-04 10:32:45 +0100

    Refactored promises

--- add.ts
+++ add.ts
@@ -4,25 +4,32 @@ export default function createAddEndpoint ( app, db ) : void {
     app.get('/add', TokenHandler.tokenAuthentication, (req,res) => {
         const ISBN = req.query.ISBN;
 
+        bookExists(db, ISBN)
+            .then ( data => {
+                //The book already exists
+
+            }, () => {
+                //The book does not exist
+                
+            })
+    }
+}
+
+function bookExists(db, ISBN) {
+    return new Promise ((resolve,reject) => {
         const isBookKnown : string = `
             SELECT *
             FROM public."BookInfo"
             WHERE "ISBN" = $1`;
 
         db.any(isBookKnown,[ISBN])
-            .then ( data => data.length > 0 )
-            .catch ( e => console.log(e) )
-            .then ( (existing : boolean) => {
-                if ( existing ) {
-                    checkBookDetailsConsistent (req.query)
-                        .then ( () => {
-
-                        }, () => {
-                            
-                        })
+            .then ( data => {
+                if ( data.length > 0 ) {
+                    resolve(data)
                 } else {
-
+                    reject()
                 }
-            });
-    } );
+            })
+            .catch(e=>console.log(e));
+    });
 }
